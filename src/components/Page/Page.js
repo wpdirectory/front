51239ed7e49c7891f8d396e9e6cc1b361884a783
Peter Booker <mail@peterbooker.com>
2020-02-06 22:22:21 +0000

    Styling tweaks

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -3,7 +3,6 @@ import './Page.scss'
 
 import Navigation from '../../components/Navigation'
 import Header from '../../components/Header'
-import Footer from '../../components/Footer'
 
 function Page({ name, children }) {
   const classes = [
@@ -20,7 +19,6 @@ function Page({ name, children }) {
           <div className="content">
             {children}
           </div>
-          <Footer />
         </div>
       </>
     </div>
